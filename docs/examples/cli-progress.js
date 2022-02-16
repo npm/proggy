dadc5a5d98841c4b4f46c5f947b40d34935fb479
Luke Karrys <luke@lukekarrys.com>
2022-02-15 21:11:02 -0700

    feat: @npmcli/template-oss@2.7.1
    BREAKING CHANGE: this drops support for node10 and non-LTS versions of
    node12 and node14

--- docs/examples/cli-progress.js
+++ docs/examples/cli-progress.js
@@ -1,21 +1,21 @@
 const chalk = require('chalk')
-const proggy = require('../')
+const proggy = require('../../lib')
+
+// note that data.actualValue and data.actualTotal will reflect the real
+// done/remaining values.  data.value will always be less than 100, and
+// data.total will always be 100, so we never show reverse motion.
+// eslint-disable-next-line max-len
+const format = `[${chalk.cyan('{bar}')}] {percentage}% | {name} {actualValue}/{actualTotal} | {duration_formatted} ETA: {eta_formatted}`
 
 // set up our display thing
 const cliProgress = require('cli-progress')
 const multibar = new cliProgress.MultiBar({
   clearOnComplete: false,
   hideCursor: true,
-  // note that data.actualValue and data.actualTotal will reflect the real
-  // done/remaining values.  data.value will always be less than 100, and
-  // data.total will always be 100, so we never show reverse motion.
-  format: '[' + chalk.cyan('{bar}') + '] {percentage}% | {name} {actualValue}/{actualTotal} | {duration_formatted} ETA: {eta_formatted}',
+  format,
   barCompleteChar: '\u2588',
   barIncompleteChar: '\u2591',
 }, cliProgress.Presets.shades_grey)
-
-
-
 
 // update it with events from the proggy client
 const client = proggy.createClient({
@@ -42,15 +42,13 @@ client.on('done', () => {
   multibar.stop()
 })
 
-
-
 // the thing that emits the events
 let b1value = 0
 let b1total = 100
 let b2value = 0
-let b2total = 2000
+const b2total = 2000
 let b3value = 0
-let b3total = 500
+const b3total = 500
 
 const b1 = proggy.createTracker('bar 1')
 const b2 = proggy.createTracker('bar 2')
@@ -62,13 +60,17 @@ const interval = setInterval(() => {
     b3value = Math.ceil(b3value + 0.003 * (b3total + b3value))
     b3.update(Math.min(b3value, b3total), b3total)
   }
-  if (2.9 * Math.random() < 1 && b1value < b1total && b1total < 2000)
-    b1total += inc*3
-  if (b1value < b1total)
+  if (2.9 * Math.random() < 1 && b1value < b1total && b1total < 2000) {
+    b1total += inc * 3
+  }
+  if (b1value < b1total) {
     b1.update(Math.min(b1total, b1value += inc), b1total)
-  if (b2value < b2total)
+  }
+  if (b2value < b2total) {
     b2.update(Math.min(b2total, b2value += inc), b2total)
+  }
 
-  if (b1value >= b1total && b2value>=b2total && b3value>=b3total)
+  if (b1value >= b1total && b2value >= b2total && b3value >= b3total) {
     clearInterval(interval)
+  }
 }, 50)
